772e16eecd59bbc577e87f8632bac0aa895421ff
Runkana Durga Prasad <runkanaprasad1@gmail.com>
2023-12-16 06:13:52 +0530

    Console statement changed

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 function hello() {
-  console.log("Hello World");
+  console.log("Hello NodeJs");
 }
 hello();
+
+// REPL Commands (READ EVAL PRINT LOOP)
+/*
+ * Multiline mode for functions
+ * Auto-Completion: Hit the Tab Key twice
+ * UnderScore character: The last value can be retrieved // useful in string interpolation
+ * "Suma"
+ * `My name is ${_}`
+ * .save name_to_store
+ *  .load mysession
+ */
